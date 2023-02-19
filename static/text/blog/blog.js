6bcee6974d27c363618ab8f6a857ad9f1beef042
The Punslinger <59809715+Pun-slinger@users.noreply.github.com>
2023-02-19 04:38:12 -0500

    blog 3

--- static/text/blog/blog.js
+++ static/text/blog/blog.js
@@ -1,38 +1,67 @@
 var fs = require('fs');
-
 var blogs = [
-    {title: "Web Usability",
-    date: "9th February 2023",
-    content: ""},
+    {
+        title: "Web Usability",
+        date: "9th February 2023",
+        content: ""
+    },
 
-    {title: "Images in creating Multimedia",
-    date: "16th February 2023",
-    content: ""},
+    {
+        title: "Images in creating Multimedia",
+        date: "16th February 2023",
+        content: ""
+    },
+
+    {
+        title: "Sound and audio in creating Multimedia",
+        date: "18th February 2023",
+        content: ""
+    },
 ]
 
 var dataArray = [];
 
-async function someFunction() {
-    for (var i = 1; i < 3; i++) {
+let fileCountPromise = new Promise(function (resolve, reject) {
+    try {
+        fs.readdir(__dirname, (err, files) => {
+            console.log(files.length);
+            resolve(files.length)
+        })
+    } catch (e) {
+        console.log("Count file failed");
+        reject(0);
+    }
+})
+
+async function someFunction(count) {
+    console.log("Count " + count);
+    for (var i = 1; i < count; i++) {
         try {
             console.log(__dirname);
-            let data = await fs.promises.readFile(__dirname + '/blog'+i+'.html', 'utf-8');
+            let data = await fs.promises.readFile(__dirname + '/blog' + i + '.html', 'utf-8');
             dataArray.push(data);
-        } catch(e) {
+        } catch (e) {
             // handle error
             console.log("Read file " + i + " failed")
 
         }
     }
     // all files have been processed here
-    for(var y = 0; y < 2; y++){
+    for (var y = 0; y < count-1; y++) {
         blogs[y].content = dataArray[y];
     }
 }
 
-someFunction().then(
-    function(value) {console.log("Done");},
-    function(error) {console.log(error);}
+fileCountPromise.then(
+    function (value) {
+        someFunction(value).then(
+            function (value) { console.log("Done"); },
+            function (error) { console.log(error); }
+        );
+    },
+    function (error) {
+        console.log("Error code " + error);
+    }
 );
 
 module.exports = {
